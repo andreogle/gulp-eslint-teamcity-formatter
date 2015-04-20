211ce265d9986b2562a454bb41b470aeee645fa5
Andre Ogle <ogleandre@yahoo.com>
2015-04-20 09:45:42 +0200

    formatted formatter.js

--- src/formatter.js
+++ src/formatter.js
@@ -15,19 +15,19 @@
  * @returns {string} An error message formatted for display in TeamCity
  */
 function escapeTeamCityString(str) {
-    if (!str) {
-        return '';
-    }
+  if (!str) {
+    return '';
+  }
 
-    return str.replace(/\|/g, '||')
-      .replace(/\'/g, '|\'')
-      .replace(/\n/g, '|n')
-      .replace(/\r/g, '|r')
-      .replace(/\u0085/g, '|x')
-      .replace(/\u2028/g, '|l')
-      .replace(/\u2029/g, '|p')
-      .replace(/\[/g, '|[')
-      .replace(/\]/g, '|]');
+  return str.replace(/\|/g, '||')
+    .replace(/\'/g, '|\'')
+    .replace(/\n/g, '|n')
+    .replace(/\r/g, '|r')
+    .replace(/\u0085/g, '|x')
+    .replace(/\u2028/g, '|l')
+    .replace(/\u2029/g, '|p')
+    .replace(/\[/g, '|[')
+    .replace(/\]/g, '|]');
 }
 
 var reportName = 'ESLint Violations';
@@ -38,40 +38,40 @@ var reportName = 'ESLint Violations';
 
 module.exports = function(results) {
 
-    var output = '',
-        summaryColor = 'yellow';
+  var output = '',
+      summaryColor = 'yellow';
 
-    output += '##teamcity[testSuiteStarted name=\'' + reportName + '\']\n';
+  output += '##teamcity[testSuiteStarted name=\'' + reportName + '\']\n';
 
-    results.forEach(function (result) {
-        var messages = result.messages;
+  results.forEach(function(result) {
+    var messages = result.messages;
 
-        if (messages.length === 0) {
-            return;
-        }
+    if (messages.length === 0) {
+      return;
+    }
 
-        output += '##teamcity[testStarted name=\'' + reportName + ':' +
-                   escapeTeamCityString(result.filePath) + '\']\n';
+    output += '##teamcity[testStarted name=\'' + reportName + ':' +
+               escapeTeamCityString(result.filePath) + '\']\n';
 
-        var messageList = [];
+    var messageList = [];
 
-        messages.forEach(function(message) {
-          if (message.severity === 2) {
-            messageList.push('line ' + message.line + ', col ' + message.column + ', ' + message.message);
-          }
-        });
-
-        if (messageList.length) {
-            output += '##teamcity[testFailed name=\'' + reportName +
-              ': ' + escapeTeamCityString(result.filePath) + '\' message=\'' + escapeTeamCityString(messageList.join('\n')) + '\']\n';
-        }
-
-        output += '##teamcity[testFinished name=\'' + reportName + ': ' + escapeTeamCityString(result.filePath) + '\']\n';
+    messages.forEach(function(message) {
+      if (message.severity === 2) {
+        messageList.push('line ' + message.line + ', col ' + message.column + ', ' + message.message);
+      }
     });
 
-    output += '##teamcity[testSuiteFinished name=\'' + reportName + '\']\n';
+    if (messageList.length) {
+      output += '##teamcity[testFailed name=\'' + reportName +
+        ': ' + escapeTeamCityString(result.filePath) + '\' message=\'' + escapeTeamCityString(messageList.join('\n')) + '\']\n';
+    }
 
-    return output;
+    output += '##teamcity[testFinished name=\'' + reportName + ': ' + escapeTeamCityString(result.filePath) + '\']\n';
+  });
+
+  output += '##teamcity[testSuiteFinished name=\'' + reportName + '\']\n';
+
+  return output;
 };
 
 
